c1c99bfd60de4937a77ae0e9026af5956e57ceed
Farooque <farooque.reactjs@gmail.com>
2025-04-05 15:17:59 +0530

    demo ready

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Header from '../components/Header'
-import SpecialityMenu from '../components/SpecialityMenu'
-import TopDoctors from '../components/TopDoctors'
-import Banner from '../components/Banner'
+import React from "react";
+import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
+import TopDoctors from "../components/TopDoctors";
+import Banner from "../components/Banner";
+import About from "./About";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <SpecialityMenu />
-      <TopDoctors />
+      {/* <SpecialityMenu /> */}
+      {/* <TopDoctors /> */}
+      <About />
       <Banner />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
